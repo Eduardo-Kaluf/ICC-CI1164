f656dc9d0595e21205d25327ecf9599746ec3f77
Eduardo Kaluf <eduardo@kaluf.com.br>
2025-09-08 22:07:19 -0300

    silent makes

--- Trab2/teste.sh
+++ Trab2/teste.sh
@@ -2,14 +2,16 @@
 
 
 TEMP_INPUT_FILE=$(mktemp)
+VOID_FILE="VOID_FILE"
 
 trap 'rm -f -- "$TEMP_INPUT_FILE"' EXIT
+trap 'rm -f -- "$VOID_FILE"' EXIT
 
 cat > "$TEMP_INPUT_FILE"
 
 # _____________ #
 
-make all
+make all &> "$VOID_FILE"
 
 ./resolveEDO < "$TEMP_INPUT_FILE"
 
@@ -17,7 +19,7 @@ make purge
 
 # _____________ #
 
-make lik-all
+make lik-all &> "$VOID_FILE"
 
 likwid-perfctr -C 3 -g FLOPS_DP -m ./resolveEDO-likwid < "$TEMP_INPUT_FILE"| grep -e "FP_ARITH_INST_RETIRED_SCALAR_DOUBLE"
 
