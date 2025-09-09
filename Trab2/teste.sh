2ab33dccf5ae04d304b9f20609e95c5b9c162422
Eduardo Kaluf <eduardo@kaluf.com.br>
2025-09-08 22:01:57 -0300

    tester again

--- Trab2/teste.sh
+++ Trab2/teste.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-make lik-all
 
 TEMP_INPUT_FILE=$(mktemp)
 
@@ -8,7 +7,17 @@ trap 'rm -f -- "$TEMP_INPUT_FILE"' EXIT
 
 cat > "$TEMP_INPUT_FILE"
 
-./resolveEDO-likwid < "$TEMP_INPUT_FILE"
+# _____________ #
+
+make all
+
+./resolveEDO < "$TEMP_INPUT_FILE"
+
+make purge
+
+# _____________ #
+
+make lik-all
 
 likwid-perfctr -C 3 -g FLOPS_DP -m ./resolveEDO-likwid < "$TEMP_INPUT_FILE"| grep -e "FP_ARITH_INST_RETIRED_SCALAR_DOUBLE"
 
