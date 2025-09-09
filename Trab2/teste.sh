2ebc6eb4b2891ae763b6483d7ad80d14fd80c0a7
Eduardo Kaluf <eduardo@kaluf.com.br>
2025-09-08 21:57:13 -0300

    Changing tester

--- Trab2/teste.sh
+++ Trab2/teste.sh
@@ -2,6 +2,15 @@
 
 make lik-all
 
-likwid-perfctr -C 3 -g FLOPS_DP -m ./resolveEDO-likwid | tee >(grep -e "FP_ARITH_INST_RETIRED_SCALAR_DOUBLE")
+TEMP_INPUT_FILE=$(mktemp)
+
+trap 'rm -f -- "$TEMP_INPUT_FILE"' EXIT
+
+cat > "$TEMP_INPUT_FILE"
+
+./resolveEDO-likwid < "$TEMP_INPUT_FILE"
+
+likwid-perfctr -C 3 -g FLOPS_DP -m ./resolveEDO-likwid < "$TEMP_INPUT_FILE"| grep -e "FP_ARITH_INST_RETIRED_SCALAR_DOUBLE"
 
 make purge
+
