832053979dc1ed49225ac543583c1f4819a4240e
sergingameprayss <sergio.sivonei@ufpr.br>
2025-10-11 07:22:20 -0300

    [add] commented matriz.h and sislin.h

--- Trab1/gradiente_conjugado.h
+++ Trab1/gradiente_conjugado.h
@@ -3,9 +3,32 @@
 
 #include "utils.h"
 
-
+/**
+ * @brief Calculo do metodo do gradiente conjulgado
+ *
+ * @param A Matriz transposta
+ * @param B Vetor termos independentes 
+ * @param X Vetor resultado do sistema
+ * @param M Matriz inversa de pre condicionadores
+ * @param n Quantidade de linhas e colunas
+ * @param maxit Maximo de iterações
+ * @param episilon Tolerancia da norma
+ * @param tempo Ponteiro para a varivel tempo 
+ *              (guarda o tempo medio das iterações)
+ * 
+ * @return norma maxima 
+ */
 real_t calc_gradiente_conjugado(real_t **A, real_t *B, real_t *X, real_t **M, int n, int maxit, real_t epsilon, rtime_t *tempo);
 
+/**
+ * @brief calcula a norma maxima
+ *
+ * @param X Novo vetor calculado
+ * @param X_old Vetor da iteração anterior
+ * @param n Tamanho do vetor
+ * 
+ * @return norma maxima entre os vetores
+ */
 real_t calc_norm(real_t *X, real_t *X_old, int n);
 
 #endif // __GRADIENTE_CONJUGADO_H__
--- Trab1/matriz.h
+++ Trab1/matriz.h
@@ -3,23 +3,90 @@
 
 #include "utils.h"
 
-
+/**
+ * @brief Aloca uma matriz fazendo as verificações de segurança necessárias
+ *
+ * @param m Matriz a ser alocada
+ * @param n Quantidade de linhas e colunas
+ *
+ */
 void alloc_single_matrix(real_t ***m, int n);
 
+/**
+ * @brief Desaloca uma matriz 
+ *
+ * @param m Matriz a ser alocada
+ * @param n Quantidade de linhas e colunas (nxn)
+ *
+ */
 void free_matrix(real_t **m, int n);
 
+/**
+ *
+ *  @brief Preenche um vetor inteiro com zeros
+ *
+ *  @param m matriz
+ *  @param n Quantidade de linhas e colunas
+ */
 void fill_zeros_matrix(real_t **m, int n);
 
-void copy_matrix(real_t **A, real_t **B, int n);
+/**
+ *
+ *  @brief Copia uma matriz para outra 
+ *
+ *  @param A matriz original a ser copiada
+ *  @param dest matriz copia  
+ *  @param n Quantidade de linhas e colunas
+ */
+void copy_matrix(real_t **A, real_t **dest, int n);
 
+/**
+ * @brief Exibe a matriz na tela com format "%.16g"
+ *
+ * @param m matriz
+ * @param n Quantidade de linhas e colunas
+ */
 void print_matrix(real_t **m, int n);
 
-void sum_matrix (real_t **A, real_t **B, int n, real_t **C);
+/**
+ * @brief Soma duas matrizes 
+ *
+ * @param A Primeira matriz a ser somada
+ * @param B Segunda matriz a ser somada
+ * @param dest matriz resultado
+ * @param n Quantidade de linhas e colunas
+ */
+void sum_matrix (real_t **A, real_t **B, int n, real_t **dest);
 
+/**
+ * @brief Multiplica uma matriz por um valor escalar 
+ *
+ * @param A Matriz original
+ * @param value valor escalar 
+ * @param dest matriz resultado
+ * @param n Quantidade de linhas e colunas
+ */
 void matrix_times_scalar(real_t **A, int n, real_t value, real_t **dest);
 
+/**
+ * @brief Multiplica uma matriz por um vetor 
+ *
+ * @param A Matriz original
+ * @param v vetor 
+ * @param rv vetor resultado
+ * @param n Tamanho do vetor e 
+ *          Quantidade de linhas e colunas da matriz
+ */
 void matrix_times_vector(real_t **m, int n, real_t *v, real_t *rv);
 
-void matrix_times_matrix(real_t **A, real_t **B, int n, real_t **C);
+/**
+ * @brief Multiplica uma matriz por uma matriz
+ *
+ * @param A Primeira matriz operando
+ * @param B Segunda matriz operando 
+ * @param dest matriz resultado
+ * @param n Quantidade de linhas e colunas
+ */
+void matrix_times_matrix(real_t **A, real_t **B, int n, real_t **dest);
 
 #endif // __MATRIZ_H__
